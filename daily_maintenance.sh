3f9fc636e71ecc8ca24bc6ec8f4651b7c9fa47a4
boronji <boronji@ik1-429-46559.vs.sakura.ne.jp>
2020-10-28 13:33:51 +0900

    fix script

--- daily_maintenance.sh
+++ daily_maintenance.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 # remove old media/preview_card
-docker-compose exec web bundle exec bin/tootctl media remove
-docker-compose exec web bundle exec bin/tootctl media remove-orphans
-docker-compose exec web bundle exec bin/tootctl preview_cards remove
+docker-compose exec -T web bundle exec bin/tootctl media remove
+docker-compose exec -T web bundle exec bin/tootctl media remove-orphans
+docker-compose exec -T web bundle exec bin/tootctl preview_cards remove
 
 # restart all services
 docker-compose down
