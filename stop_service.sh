b9dbb2bfa733fbf2a24040bcd712e757942a6fc5
boronology <boronology@gmail.com>
2023-06-21 22:54:11 +0900

    docker-compose -> docker compose

--- stop_service.sh
+++ stop_service.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker-compose down
-snapper -c config create -c number --description 'manual down' 
+docker compose down
+snapper -c mastodon-dir create -c number --description 'manual down'
